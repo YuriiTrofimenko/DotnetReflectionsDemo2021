33da75c4379cb42c8f514638716a06b6b261413d
Student <Student@DESKTOP-6THNPEV>
2021-07-01 20:13:39 +0300

    init n demo

---
+++ ReflectionsDemo/Surprise.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ReflectionsDemo
+{
+    class Surprise
+    {
+        public static dynamic GetResult() {
+
+            return null;
+        }
+    }
+}
